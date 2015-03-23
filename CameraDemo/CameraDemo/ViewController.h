c905bdb23257cddf4eb69aab8d2007a457e44cda
pinguo-xiongmingxue <xiongmingxue@camera.com>
2015-03-23 18:10:56 +0800

    init

---
+++ CameraDemo/CameraDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CameraDemo
+//
+//  Created by pinguo on 15/3/23.
+//  Copyright (c) 2015年 pinguo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
